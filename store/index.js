978b2df1720b8d387bb0126996d23a9793cd147d
Hauss <Hauss@HAUSS>
2020-01-14 17:02:23 +0800

    update api

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue"
+import Vuex from "vuex"
 
 Vue.use(Vuex)
 
@@ -45,75 +45,92 @@ const store = new Vuex.Store({
 				// } else {
 				// 	uni.login({
 				// 		success: (data) => {
-				// 			commit('login')
+				// 			commit("login")
 				// 			setTimeout(function() { //模拟异步请求服务器获取 openid
-				// 				const openid = '123456789'
-				// 				console.log('uni.request mock openid[' + openid + ']');
-				// 				commit('setOpenid', openid)
+				// 				const openid = "123456789"
+				// 				console.log("uni.request mock openid[" + openid + "]");
+				// 				commit("setOpenid", openid)
 				// 				resolve(openid)
 				// 			}, 1000)
 				// 		},
 				// 		fail: (err) => {
-				// 			console.log('uni.login 接口调用失败，将无法正常使用开放接口等服务', err)
+				// 			console.log("uni.login 接口调用失败，将无法正常使用开放接口等服务", err)
 				// 			reject(err)
 				// 		}
 				// 	})
 				// }
-				let selected = []
-				console.log(date + ":date")
-				if (date === 1) {
-					selected = [{
-							date: '2019-11-21',
-							info: '签到2',
-							days: '100'
-						},
-						{
-							date: '2019-11-21',
-							info: '签到1',
-							days: '100'
-						},
-						{
-							date: '2019-11-21',
-							info: '签到2',
-							days: '100'
-						},
-						{
-							date: '2019-11-21',
-							info: '签到1',
-							days: '100'
-						},
-						{
-							date: '2019-11-21',
-							info: '签到2',
-							days: '100'
-						},
-						{
-							date: '2019-11-21',
-							info: '签到1',
-							days: '100'
-						}
-					]
-				} else {
-					selected = [{
-							date: '2019-08-20',
-							info: '打卡',
-							days: '100'
-						},
-						{
-							date: '2019-08-21',
-							info: '签到',
-							days: '100'
-						},
-						{
-							date: '2019-08-22',
-							info: '已打卡',
-							days: '100'
-						}
-					]
-				}
-
-				commit('setCurrentMonthSelected', selected)
-				resolve(selected)
+				
+				
+				
+				
+				// let selected = []
+				// console.log(date + ":date")
+				// if (date === 1) {
+				// 	selected = [
+				// 		{
+				// 			"date": "2019-11-21",
+				// 			"info": "签到2",
+				// 			"days": "100"
+				// 		},
+				// 		{
+				// 			"date": "2019-11-21",
+				// 			"info": "签到1",
+				// 			"days": "100"
+				// 		},
+				// 		{
+				// 			"date": "2019-11-21",
+				// 			"info": "签到2",
+				// 			"days": "100"
+				// 		},
+				// 		{
+				// 			"date": "2019-11-21",
+				// 			"info": "签到1",
+				// 			"days": "100"
+				// 		},
+				// 		{
+				// 			"date": "2019-11-21",
+				// 			"info": "签到2",
+				// 			"days": "100"
+				// 		},
+				// 		{
+				// 			"date": "2019-11-21",
+				// 			"info": "签到1",
+				// 			"days": "100"
+				// 		}
+				// 	]
+				// } else {
+				// 	selected = [{
+				// 			"date": "2019-08-20",
+				// 			"info": "打卡",
+				// 			"days": "100"
+				// 		},
+				// 		{
+				// 			"date": "2019-08-21",
+				// 			"info": "签到",
+				// 			"days": "100"
+				// 		},
+				// 		{
+				// 			"date": "2019-08-22",
+				// 			"info": "已打卡",
+				// 			"days": "100"
+				// 		}
+				// 	]
+				// }
+				uni.request({
+					url: 'http://www.mocky.io/v2/5e1d8185360000521dc740e8',
+				}).then(res => {
+					console.log('request success', JSON.stringify(res[1].data));
+					const selected = res[1].data
+					commit("setCurrentMonthSelected", selected)
+					resolve(selected)
+				}).catch(err => {
+					console.log('request fail', err);
+					uni.showModal({
+						content: err.errMsg,
+						showCancel: false
+					});
+					reject(err)
+				});			
 			})
 		}
 	}
